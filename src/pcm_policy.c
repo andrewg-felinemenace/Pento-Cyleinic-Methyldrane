3d78aa9d2ca5fd8c8c965f9c905583bfb05ab11a
Andrew Griffiths <andrewg@felinemenace.org>
2013-12-08 20:02:20 +1100

    Refactor, support argument restrictions in src/pcm_policy.c
    Split function into 3 pieces to make code easier to read.

--- src/pcm_policy.c
+++ src/pcm_policy.c
@@ -38,11 +38,152 @@ u_int32_t pcm_string_to_policy(const char *str)
 	return -1;
 }
 
+
+// this is the maximum argument count supported ..
+//
+// ../src/arch-x32.h:#define x32_arg_count_max             6
+// ../src/arch-x86.h:#define x86_arg_count_max             6
+// ../src/arch.h:#define ARG_COUNT_MAX     6
+// ../src/arch-arm.h:#define arm_arg_count_max             6
+// ../src/arch-x86_64.h:#define x86_64_arg_count_max               6
+
+#ifndef ARG_COUNT_MAX
+#define ARG_COUNT_MAX 6
+#endif
+
+int pcm_json_op_to_define(const char *op)
+{
+	// As from the documentation, 
+
+	// SCMP_CMP_NE = 1,                /**< not equal */
+        // SCMP_CMP_LT = 2,                /**< less than */
+        // SCMP_CMP_LE = 3,                /**< less than or equal */
+        // SCMP_CMP_EQ = 4,                /**< equal */
+        // SCMP_CMP_GE = 5,                /**< greater than or equal */
+        // SCMP_CMP_GT = 6,                /**< greater than */
+        // SCMP_CMP_MASKED_EQ = 7,         /**< masked equality */
+
+	if(strcasecmp(op, "ne") == 0) return SCMP_CMP_NE;
+	if(strcasecmp(op, "lt") == 0) return SCMP_CMP_LT;
+	if(strcasecmp(op, "le") == 0) return SCMP_CMP_LE;
+	if(strcasecmp(op, "eq") == 0) return SCMP_CMP_EQ;
+	if(strcasecmp(op, "ge") == 0) return SCMP_CMP_GE;
+	if(strcasecmp(op, "gt") == 0) return SCMP_CMP_GT;
+	if(strcasecmp(op, "masked_eq") == 0) return SCMP_CMP_MASKED_EQ;
+
+	errx(EXIT_FAILURE, "json operation value is not handled here - %s", op);
+
+}
+
+void pcm_json_restriction_to_arg_cmp(json_t *restriction, struct scmp_arg_cmp *arg_cmp)
+{
+	json_t *arg, *op, *datum_a, *datum_b;
+
+	// XXX, test cases for datum_* handling / jansson integer handling etc.
+
+	arg = json_object_get(restriction, "arg");
+	op = json_object_get(restriction, "op");
+	datum_a = json_object_get(restriction, "datum_a");
+	datum_b = json_object_get(restriction, "datum_b");
+
+	if(! json_is_integer(arg)) {
+		errx(EXIT_FAILURE, "syscall argument number is not an integer");
+	}
+
+	if(! json_is_string(op)) {
+		errx(EXIT_FAILURE, "operation value is not a string");
+	}
+
+	if(! json_is_integer(datum_a)) {
+		errx(EXIT_FAILURE, "datum_a is not an integer");
+	}
+
+	if(datum_b) {
+		if(! json_is_integer(datum_b)) {
+			errx(EXIT_FAILURE, "datum_b is not an integer");
+		}
+	}
+
+	arg_cmp->arg = json_integer_value(arg);
+	arg_cmp->op = pcm_json_op_to_define(json_string_value(op));
+	arg_cmp->datum_a = json_integer_value(datum_a);
+	arg_cmp->datum_b = datum_b ? json_integer_value(datum_b) : 0;
+}
+
+void pcm_json_rule_to_seccomp(json_t *rule)
+{
+	json_t *syscall, *action, *restrictions;
+	int rc;
+	int syscall_num;
+	int arg_count;
+	int i;
+	int default_action;
+	struct scmp_arg_cmp arg_cmp[ARG_COUNT_MAX];
+		
+		
+	syscall = json_object_get(rule, "syscall");
+	if(! json_is_string(syscall)) {
+		errx(EXIT_FAILURE, "Expected string for system call");
+	}
+
+	action = json_object_get(rule, "action");
+	if(! json_is_string(action)) {
+		errx(EXIT_FAILURE, "Expected string for action value");
+	}
+
+	default_action = pcm_string_to_policy(json_string_value(action));
+	if(default_action == -1) {
+		errx(EXIT_FAILURE, "Rule action invalid (expecting ALLOW, KILL, or ERRNO)");
+	}	
+
+	syscall_num = seccomp_syscall_resolve_name(json_string_value(syscall));
+	if(syscall_num == __NR_SCMP_ERROR) {
+		errx(EXIT_FAILURE, "System call number is negative?");
+	}
+
+	arg_count = 0;
+	restrictions = json_object_get(rule, "restrictions");
+
+	if(restrictions) {
+		if(! json_is_array(restrictions)) {
+			errx(EXIT_FAILURE, "restrictions is not an array");
+		}
+
+		if(json_array_size(restrictions) > ARG_COUNT_MAX) {
+			errx(EXIT_FAILURE, "too many restrictions :/");
+		}
+
+		for(i = 0; i < json_array_size(restrictions); i++) {
+			json_t *arg_restriction;
+			arg_restriction = json_array_get(restrictions, i);
+			if(! arg_restriction) {
+				errx(EXIT_FAILURE, "fetching entry from array failed?");
+			}
+
+			if(! json_is_object(arg_restriction)) {
+				errx(EXIT_FAILURE, "argument restriction is not an object");
+			}
+
+			pcm_json_restriction_to_arg_cmp(arg_restriction, &arg_cmp[i]);
+		}
+		arg_count = i;
+	}
+
+	rc = seccomp_rule_add_array(PCM_GLOBAL.seccomp, default_action, syscall_num, arg_count, arg_cmp);
+	if(rc < 0) {
+		errx(EXIT_FAILURE, "Adding rule failed?");
+	}
+
+
+}
+
 int pcm_json_to_seccomp(char **hook)
 {
 	json_t *str, *rules, *hook_obj;
 	u_int32_t default_action;
 	int i, rc;
+
+
 
 	if(hook) *hook = NULL; // initialize to a sane value
 
@@ -102,41 +243,15 @@ int pcm_json_to_seccomp(char **hook)
 	}
 
 	for(i = 0; i < json_array_size(rules); i++) {
-		json_t *rule, *syscall, *action;
-		int rc;
-		int syscall_num;
-		
-		
+		json_t *rule;
+
+
 		rule = json_array_get(rules, i);
 		if(! json_is_object(rule)) {
 			errx(EXIT_FAILURE, "Expected rule object, got something else instead");
 		}
 		
-		syscall = json_object_get(rule, "syscall");
-		if(! json_is_string(syscall)) {
-			errx(EXIT_FAILURE, "Expected string for system call");
-		}
-
-		action = json_object_get(rule, "action");
-		if(! json_is_string(action)) {
-			errx(EXIT_FAILURE, "Expected string for action value");
-		}
-
-		default_action = pcm_string_to_policy(json_string_value(action));
-		if(default_action == -1) {
-			errx(EXIT_FAILURE, "Rule action invalid (expecting ALLOW, KILL, or ERRNO)");
-		}	
-	
-		syscall_num = seccomp_syscall_resolve_name(json_string_value(syscall));
-		if(syscall_num == __NR_SCMP_ERROR) {
-			errx(EXIT_FAILURE, "System call number is negative?");
-		}
-
-		rc = seccomp_rule_add(PCM_GLOBAL.seccomp, default_action, syscall_num, 0);
-		if(rc < 0) {
-			errx(EXIT_FAILURE, "Adding rule failed?");
-		}
-
+		pcm_json_rule_to_seccomp(rule);
 	}
 
 	pcm_policy_free();
